6bbc606fabb8e120d093c2fc834126e27163e397
Joseph Rollins <rollins.joseph@gmail.com>
2023-06-27 21:46:00 -0700

    Use svelte simple datatables

--- src/routes/orders/[company]/+page.svelte
+++ src/routes/orders/[company]/+page.svelte
@@ -1,208 +1,91 @@
 <script lang="ts">
-  import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
-  import type { TableSource } from '@skeletonlabs/skeleton';
   import { invalidateAll } from '$app/navigation';
   import { onDestroy } from 'svelte';
   import type { PageData } from './$types';
   import { browser } from '$app/environment';
-  import { round, sortByKeys } from '$lib/utils';
+  import { round } from '$lib/utils';
+  import { Datatable, Th, DataHandler } from '@vincjo/datatables';
 
   export let data: PageData;
 
   if (browser) {
     const interval = setInterval(() => {
       invalidateAll();
-    }, 5000);
+    }, 10000);
 
     onDestroy(() => clearInterval(interval));
   }
 
-  $: tableData = data.orders
-    .flatMap((order) => {
-      return order.Orders.map((details) => {
-        const snipes =
-          details.Type === 'Buy'
-            ? order.Market.BuyingOrders.filter((otherOrder) => otherOrder.ItemCost > details.Cost)
-            : order.Market.SellingOrders.filter((otherOrder) => otherOrder.ItemCost < details.Cost);
-        return {
-          material: order.Market.MaterialTicker,
-          exchange: order.Market.ExchangeCode,
-          type: details.Type,
-          count: details.Count,
-          cost: round(details.Cost),
-          bid: round(order.Market.Bid),
-          ask: round(order.Market.Ask),
-          spread: round(order.Market.Ask - order.Market.Bid),
-          snipe: snipes.length
-            ? `${snipes.reduce((acc, o) => acc + o.ItemCount, 0)} @ ± ${
-                details.Type === 'Buy'
-                  ? round(order.Market.Bid - details.Cost)
-                  : round(details.Cost - order.Market.Ask)
-              }`
-            : ''
-        };
-      });
-    })
-    .sort(sortByKeys('material', 'exchange', 'type'));
+  const handler = new DataHandler();
+  const rows = handler.getRows();
 
-  $: tableSource = {
-    head: ['MAT', 'CX', 'TYPE', 'AMT', 'PRICE', 'BID', 'ASK', 'SPRD', 'OUTBID'],
-    body: tableMapperValues(tableData, [
-      'material',
-      'exchange',
-      'type',
-      'count',
-      'cost',
-      'bid',
-      'ask',
-      'spread',
-      'snipe'
-    ]),
-    meta: tableMapperValues(tableData, [
-      'material',
-      'exchange',
-      'type',
-      'count',
-      'cost',
-      'bid',
-      'ask',
-      'spread',
-      'snipe'
-    ])
-  };
+  $: {
+    handler.setRows(
+      data.orders.flatMap((order) => {
+        return order.Orders.map((details) => {
+          const snipes =
+            details.Type === 'Buy'
+              ? order.Market.BuyingOrders.filter((otherOrder) => otherOrder.ItemCost > details.Cost)
+              : order.Market.SellingOrders.filter(
+                  (otherOrder) => otherOrder.ItemCost < details.Cost
+                );
+          return {
+            material: order.Market.MaterialTicker,
+            exchange: order.Market.ExchangeCode,
+            type: details.Type,
+            count: details.Count,
+            cost: round(details.Cost),
+            bid: round(order.Market.Bid),
+            ask: round(order.Market.Ask),
+            spread: round(order.Market.Ask - order.Market.Bid),
+            snipe: snipes.length
+              ? `${snipes.reduce((acc, o) => acc + o.ItemCount, 0)} @ ± ${
+                  details.Type === 'Buy'
+                    ? round(order.Market.Bid - details.Cost)
+                    : round(details.Cost - order.Market.Ask)
+                }`
+              : ''
+          };
+        });
+      })
+    );
+  }
 </script>
 
 <div class="max-w-4xl mx-auto">
-  <h1 class="text-2xl pb-8 pt-8">Orders for {data.company}</h1>
-  <Table source={tableSource} />
-  <!-- <div class="table shadow-lg border border-slate-200 dark:border-slate-400/20">
-    <div class="table-header-group">
-      <div
-        class="table-cell p-4 font-medium text-left border-b-4 border-r border-slate-200 dark:border-slate-400/20"
-      >
-        MAT
-      </div>
-      <div
-        class="table-cell p-4 font-medium text-left border-b-4 border-r border-slate-200 dark:border-slate-400/20"
-      >
-        CX
-      </div>
-      <div
-        class="table-cell p-4 font-medium text-left border-b-4 border-r border-slate-200 dark:border-slate-400/20"
-      >
-        TYPE
-      </div>
-      <div
-        class="table-cell p-4 font-medium text-left border-b-4 border-r border-slate-200 dark:border-slate-400/20"
-      >
-        AMT
-      </div>
-      <div
-        class="table-cell p-4 font-medium text-left border-b-4 border-r border-slate-200 dark:border-slate-400/20"
-      >
-        PRICE
-      </div>
-      <div
-        class="table-cell p-4 font-medium text-left border-b-4 border-r border-slate-200 dark:border-slate-400/20"
-      >
-        BID
-      </div>
-      <div
-        class="table-cell p-4 font-medium text-left border-b-4 border-r border-slate-200 dark:border-slate-400/20"
-      >
-        ASK
-      </div>
-      <div
-        class="table-cell p-4 font-medium text-left border-b-4 border-r border-slate-200 dark:border-slate-400/20"
-      >
-        SPRD
-      </div>
-      <div
-        class="table-cell p-4 font-medium text-left border-b-4 border-slate-200 dark:border-slate-400/20"
-      >
-        OUTBID
-      </div>
-    </div>
-    {#each orders as order, index}
-      {@const last = index === orders.length - 1}
-      {#each order.Orders as ord}
-        {@const sniped = order.Market[ord.Type === 'Buy' ? 'Bid' : 'Ask'] !== ord.Cost}
-        {@const snipes =
-          ord.Type === 'Buy'
-            ? order.Market.BuyingOrders.filter((otherOrder) => otherOrder.ItemCost > ord.Cost)
-            : order.Market.SellingOrders.filter((otherOrder) => otherOrder.ItemCost < ord.Cost)}
-        <div class="table-row">
-          <div
-            class="table-cell p-4 text-left border-r border-slate-200 dark:border-slate-400/20"
-            class:border-b={!last}
-            class:sniped
-          >
-            {order.Ticker.split('.')[0]}
-          </div>
-          <div
-            class="table-cell p-4 text-left border-r border-slate-200 dark:border-slate-400/20"
-            class:border-b={!last}
-            class:sniped
-          >
-            {order.Ticker.split('.')[1]}
-          </div>
-          <div
-            class="table-cell p-4 text-left border-r border-slate-200 dark:border-slate-400/20"
-            class:border-b={!last}
-            class:sniped
-          >
-            {ord.Type === 'Buy' ? 'BUY' : 'SELL'}
-          </div>
-          <div
-            class="table-cell p-4 text-left border-r border-slate-200 dark:border-slate-400/20"
-            class:border-b={!last}
-            class:sniped
-          >
-            {ord.Count}
-          </div>
-          <div
-            class="table-cell p-4 text-left border-r border-slate-200 dark:border-slate-400/20"
-            class:border-b={!last}
-            class:sniped
-          >
-            {round(ord.Cost)}
-          </div>
-          <div
-            class="table-cell p-4 text-left border-r border-slate-200 dark:border-slate-400/20"
-            class:border-b={!last}
-            class:sniped
-          >
-            {round(order.Market.Bid)}
-          </div>
-          <div
-            class="table-cell p-4 text-left border-r border-slate-200 dark:border-slate-400/20"
-            class:border-b={!last}
-            class:sniped
-          >
-            {round(order.Market.Ask)}
-          </div>
-          <div
-            class="table-cell p-4 text-left border-r border-slate-200 dark:border-slate-400/20"
-            class:border-b={!last}
-            class:sniped
-          >
-            {round(order.Market.Ask - order.Market.Bid)}
-          </div>
-          <div
-            class="table-cell p-4 text-left border-slate-200 dark:border-slate-400/20"
-            class:border-b={!last}
-            class:sniped
-          >
-            {#if sniped}
-              {snipes.reduce((acc, o) => acc + o.ItemCount, 0)} @ ±{ord.Type === 'Buy'
-                ? round(order.Market.Bid - ord.Cost)
-                : round(ord.Cost - order.Market.Ask)}
-            {/if}
-          </div>
-        </div>
-      {/each}
-    {/each}
-  </div> -->
+  <h1 class="text-2xl pb-4 pt-8">Orders for {data.company}</h1>
+  <Datatable {handler} rowsPerPage={false} pagination={false} rowCount={false}>
+    <table>
+      <thead>
+        <tr>
+          <Th {handler} orderBy="material">MAT</Th>
+          <Th {handler} orderBy="exchange">CX</Th>
+          <Th {handler} orderBy="type">TYPE</Th>
+          <Th {handler} orderBy="count">AMT</Th>
+          <Th {handler} orderBy="cost">COST</Th>
+          <Th {handler} orderBy="bid">BID</Th>
+          <Th {handler} orderBy="ask">ASK</Th>
+          <Th {handler} orderBy="spread">SPRD</Th>
+          <Th {handler} orderBy="snipe">OUTBID</Th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each $rows as row}
+          <tr class:sniped={!!row.snipe}>
+            <td>{row.material}</td>
+            <td>{row.exchange}</td>
+            <td>{row.type}</td>
+            <td>{row.count}</td>
+            <td>{row.cost}</td>
+            <td>{row.bid}</td>
+            <td>{row.ask}</td>
+            <td>{row.spread}</td>
+            <td>{row.snipe}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </Datatable>
 </div>
 
 <style lang="postcss">
